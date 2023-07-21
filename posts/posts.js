2d0826df3d4d10d61a41f3ef81a5112eab917c80
Tatiana <tstwrt7@gmail.com>
2023-07-21 02:45:38 -0400

    Added "Post Type" feature

---
+++ posts/posts.js
@@ -0,0 +1,143 @@
+/* Posts Page JavaScript */
+
+"use strict";
+
+const logoutButton = document.querySelector("#logout");
+const postContainer = document.querySelector('#postContainer');
+logoutButton.onclick = logout;
+
+function convertDateTime(apiDateTime) {
+    const date = new Date(apiDateTime);
+    const formattedDateTime = date.toLocaleString();
+    return formattedDateTime;
+}
+
+//LIKES FEATURE
+function countLikes(likes) {
+    return likes.length;
+}
+
+function postFetch() {
+    const loginData = getLoginData();
+
+    const options = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
+
+    fetch(apiBaseURL + "/api/posts", options)
+        .then(response => response.json())
+        .then(posts => {
+            posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+            posts.forEach(post => {
+                if(window.localStorage.getItem(post._id) === null) {
+                    const cardHTML = `
+                    <div class="card text-center" id="cards" data-post-id="${post._id}">
+                    <div class="card-header">
+                    <b>@${post.username}</b>
+                    </div>
+                    <div class="card-body">
+                    <p class="card-text">${post.text}</p>
+                    </div><br>
+                    <div class="card-footer text-muted">
+                    ${convertDateTime(post.createdAt)}<br>
+                    <span class="likes-count">${countLikes(post.likes)} Likes</span>
+                    <button onmouseover="mouseOverEffect('${post._id}','${post.likes}')" onmouseout="mouseOutEffect('${post._id}')" class="like-button" id="${post._id}" onclick="likedOrNah('${post._id}')">❤</button>
+                    </div>
+                </div>`;
+                    postContainer.innerHTML += cardHTML;
+                } else {
+                    const cardHTML = `
+                    <div class="card text-center" id="cards" data-post-id="${post._id}">
+                    <div class="card-header">
+                    <b>@${post.username}</b>
+                    </div>
+                    <div class="card-body">
+                    <p class="card-text" >${post.text}</p>
+                    </div><br>
+                    <div class="card-footer text-muted">
+                    ${convertDateTime(post.createdAt)}<br>
+                    <span class="likes-count">${countLikes(post.likes)} Likes</span>
+                    <button onmouseover="mouseOverEffect('${post._id}','${post.likes}')" onmouseout="mouseOutEffect('${post._id}')" class="like-button liked" id="${post._id}" onclick="likedOrNah('${post._id}')">❤</button>
+                    </div>
+                    </div>`;
+                    postContainer.innerHTML += cardHTML;
+                }
+            });
+            
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
+    
+    window.onload = postFetch;
+    
+    function likedOrNah(postId) {
+        if (window.localStorage.getItem(postId) === null) {
+            toggleLike(postId)
+        } else {
+            untoggleLike(postId)
+        }
+    }
+    
+    function toggleLike(postId) {
+        const loginData = getLoginData();
+        const likeButton = document.querySelector(`button[id='${postId}']`)
+        likeButton.classList.toggle('liked')
+        const options = {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${loginData.token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ postId: postId }),
+        };
+        
+        fetch(apiBaseURL + "/api/likes", options)
+        .then((response) => response.json())
+    .then((data) => {
+        window.localStorage.setItem(data.postId,data._id)
+        window.location.reload()
+    });
+    
+}
+
+function untoggleLike(postId) {
+    const loginData = getLoginData();
+    const likeButton = document.querySelector(`button[id='${postId}']`)
+    likeButton.classList.toggle('liked')
+    const endpoint = window.localStorage.getItem(postId)
+    const options = {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+            "Content-Type": "application/json",
+        },
+    };
+    fetch(apiBaseURL + "/api/likes/" + endpoint, options)
+    .then((response) => response.json())
+    .then((data) => {
+        window.localStorage.removeItem(postId)
+        window.location.reload()
+    });
+}
+
+
+function mouseOverEffect(postId,postLikes) {
+    console.log(postId)
+    console.log(postLikes)
+    // postLikes.forEach(a=>console.log(a.username))
+    // postLikes.forEach(a=>console.log(a.username))
+    const likeButton = document.querySelector(`button[id='${postId}']`)
+    likeButton.classList.add('mousedOver')
+}
+
+function mouseOutEffect(postId) {
+    const likeButton = document.querySelector(`button[id='${postId}']`)
+    likeButton.classList.remove('mousedOver')
+}
+
---
+++ profile/profile.js
@@ -0,0 +1,223 @@
+"use strict";
+
+const logoutButton = document.querySelector("#logout");
+const profileContainer = document.querySelector("#profileContainer");
+const userPost = document.querySelector("#userPost");
+const dataContainer = document.querySelector("#userDataContainer");
+const bioDisplay = document.querySelector("#bioDisplay");
+const saveBioButton = document.querySelector("#saveBio");
+const bioTextarea = document.querySelector("#biotext");
+
+userPost.onsubmit = formSubmit;
+logoutButton.onclick = logout;
+saveBioButton.onclick = saveBio;
+
+function convertDateTime(apiDateTime) {
+  const date = new Date(apiDateTime);
+  const formattedDateTime = date.toLocaleString();
+  return formattedDateTime;
+}
+
+//Display Your Posts
+function profileFetch() {
+  const loginData = getLoginData();
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+    },
+  };
+
+  fetch(apiBaseURL + "/api/posts?username=" + loginData.username, options)
+    .then((response) => response.json())
+    .then((userProfiles) => {
+      userProfiles.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+      let postHTML = "";
+
+      userProfiles.forEach((userProfile) => {
+        postHTML += `
+        <div class="card text-center" id="cards" data-post-id="${userProfile._id}">
+          <div class="card-header">
+          <span class="deleteButton"><button class="btn btn-outline-danger" id="${userProfile._id}" onclick="deletePost('${userProfile._id}')">Χ</button></span>
+              <b>@${userProfile.username}</b>
+          </div>
+          <div class="card-body">
+              <p class="card-text">${userProfile.text}</p>
+          </div><br>
+          <div class="card-footer text-muted">
+              ${convertDateTime(userProfile.createdAt)}
+          </div>
+        </div>`;
+      });
+    profileContainer.innerHTML = postHTML;
+    })
+      .catch((error) => {
+        console.error(error);
+      });
+}
+
+
+//Display User Info
+function displayUserData() {
+
+  const loginData = getLoginData();
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+    },
+  };
+
+  fetch(apiBaseURL + "/api/users/" + loginData.username, options)
+    .then(response => response.json())
+    .then(userData => {
+        
+        const data = `
+        <h4> <b>${userData.fullName}</b></h4>
+        <div class = "username">@${userData.username}</div>`;
+        dataContainer.innerHTML = data;
+
+        const bio = `${userData.bio}`;
+        bioDisplay.textContent = bio || "No bio yet!";
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+
+//Update User Bio
+function updateBio() {
+  const loginData = getLoginData();
+  const newBio = bioTextarea.value;
+  const options = {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({bio: newBio}),
+  };
+
+  fetch(apiBaseURL + "/api/users/" + loginData.username, options)
+    .then(response => {
+      displayUserData();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function saveBio() {
+  if (!bioDisplay || !bioTextarea || !saveBioButton) {
+    return; // Elements not found, exit the function
+  }
+
+  bioDisplay.textContent = bioTextarea.value;
+  bioDisplay.style.display = "block";
+  bioTextarea.style.display = "none";
+  saveBioButton.style.display = "none";
+  updateBio();
+}
+
+//Form Submit
+function formSubmit(event) {
+  event.preventDefault();
+  profileContainer.replaceChildren();
+  sendData(userPost.userPostArea.value);
+}
+
+//Send Data
+function sendData(postContent) {
+  const loginData = getLoginData();
+  console.log(loginData);
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: postContent }),
+  };
+
+  fetch(apiBaseURL + "/api/posts", options)
+    .then((response) => response.json())
+    .then((data) => {
+      profileFetch();
+      console.log(data);
+    });
+}
+
+window.onload = main;
+
+function main() {
+  profileFetch();
+  displayUserData();
+}
+
+//DELETE POSTS
+function deletePost(postId) {
+    const loginData = getLoginData();
+    const options = {
+      method: "DELETE",
+      headers: {
+      Authorization: `Bearer ${loginData.token}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+    fetch(apiBaseURL + "/api/posts/" + postId, options)
+    .then((response) => response.json())
+    .then((data) => {
+      window.location.reload()
+    });
+}
+
+// SELECT POST TYPE
+const postOption = document.querySelector("#postTypeDropdown");
+const audioPost = document.querySelector("#audioPost");
+const pollPost = document.querySelector("#pollPost");
+const videoPost = document.querySelector("#videoPost");
+const eventPost = document.querySelector("#eventPost");
+
+function postType() {
+  const selectedType = postOption.value;
+
+  // Hide all form sections
+  audioPost.style.display = "none";
+  pollPost.style.display = "none";
+  videoPost.style.display = "none";
+  eventPost.style.display = "none";
+
+  // Show the selected form section based on the dropdown value
+  if (selectedType === "Audio") {
+    audioPost.style.display = "block";
+    userPost.style.display = "none";
+  } else if (selectedType === "Poll") {
+    pollPost.style.display = "block";
+    userPost.style.display = "none";
+  } else if (selectedType === "Video") {
+    videoPost.style.display = "block";
+    userPost.style.display = "none";
+  } else if (selectedType === "Event") {
+    eventPost.style.display = "block";
+    userPost.style.display = "none";
+  } else if (selectedType === "Text") {
+    userPost.style.display = "block";
+  }
+}
+//Add Poll Option
+function addOption() {
+  var pollOptionsDiv = document.getElementById("pollOptions");
+  var optionNumber = pollOptionsDiv.getElementsByTagName("div").length + 1;
+
+  var newOptionDiv = document.createElement("div");
+  var newOptionInput = document.createElement("input");
+  newOptionInput.type = "text";
+  newOptionInput.name = "option" + optionNumber;
+  newOptionInput.required = true;
+  newOptionInput.classList = "pollInput";
+
+  newOptionDiv.appendChild(newOptionInput);
+  pollOptionsDiv.appendChild(newOptionDiv);
+}
